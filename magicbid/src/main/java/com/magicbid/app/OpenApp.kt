e77431c98b3891149b381e7e84df5f14032f442f
arundwivedi <arund@digislots.in>
2023-08-30 17:33:08 +0530

    TimerRemove

--- magicbid/src/main/java/com/magicbid/app/OpenApp.kt
+++ magicbid/src/main/java/com/magicbid/app/OpenApp.kt
@@ -19,10 +19,28 @@ class OpenApp : AppCompatActivity() {
 //            startActivity(intent)
 //
 //        }
+        val application = application as? App
 
+        // If the application is not an instance of MyApplication, log an error message and
+        // start the MainActivity without showing the app open ad.
+        if (application == null) {
+
+            return
+        }
+
+        // Show the app open ad.
+        application.showAdIfAvailable(
+            this@OpenApp,
+            object : App.OnShowAdCompleteListener {
+                override fun onShowAdComplete() {
+                    //startMainActivity()
+                }
+
+
+            })
 
         // Create a timer so the SplashActivity will be displayed for a fixed amount of time.
-        createTimer(5)
+        //createTimer(5)
     }
 
     /**
@@ -30,44 +48,26 @@ class OpenApp : AppCompatActivity() {
      *
      * @param seconds the number of seconds that the timer counts down from
      */
-    private fun createTimer(seconds: Long) {
-        val counterTextView: TextView = findViewById(R.id.timer)
-        val countDownTimer: CountDownTimer = object : CountDownTimer(seconds * 1000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                secondsRemaining = millisUntilFinished / 1000 + 1
-                counterTextView.text = "App is done loading in: $secondsRemaining"
-            }
-
-            override fun onFinish() {
-                secondsRemaining = 0
-                counterTextView.text = "Done."
-
-                val application = application as? App
-
-                // If the application is not an instance of MyApplication, log an error message and
-                // start the MainActivity without showing the app open ad.
-                if (application == null) {
-
-                    return
-                }
-
-                // Show the app open ad.
-                application.showAdIfAvailable(
-                    this@OpenApp,
-                    object : App.OnShowAdCompleteListener {
-                        override fun onShowAdComplete() {
-                            //startMainActivity()
-                        }
-
-
-                    })
-            }
-        }
-        countDownTimer.start()
-    }
-//    private fun startMainActivity() {
-//        val intent = Intent(this, MainActivity::class.java)
-//        startActivity(intent)
+//    private fun createTimer(seconds: Long) {
+//        val counterTextView: TextView = findViewById(R.id.timer)
+//        val countDownTimer: CountDownTimer = object : CountDownTimer(seconds * 1000, 1000) {
+//            override fun onTick(millisUntilFinished: Long) {
+//                secondsRemaining = millisUntilFinished / 1000 + 1
+//                counterTextView.text = "App is done loading in: $secondsRemaining"
+//            }
+//
+//            override fun onFinish() {
+//                secondsRemaining = 0
+//                counterTextView.text = "Done."
+//
+//
+//            }
+//        }
+//        countDownTimer.start()
 //    }
+////    private fun startMainActivity() {
+////        val intent = Intent(this, MainActivity::class.java)
+////        startActivity(intent)
+////    }
 
 }

a8342f099406223251f7bf427457fdfebdfacf8e
arundwivedi <arund@digislots.in>
2023-10-12 11:11:14 +0530

    No internet

--- magicbid/src/main/java/com/magicbid/app/App.kt
+++ magicbid/src/main/java/com/magicbid/app/App.kt
@@ -5,10 +5,14 @@ import android.app.Application
 import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
 import android.text.format.Formatter
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
@@ -53,34 +57,61 @@ open class App : Application(), Application.ActivityLifecycleCallbacks, Lifecycl
             .getApplicationInfo(applicationContext.packageName, PackageManager.GET_META_DATA)
         val value = ai.metaData["token"]
 
-        CoroutineScope(Dispatchers.IO).launch {
+        if (checkForInternet(this)) {
+            CoroutineScope(Dispatchers.IO).launch {
 
 
-            try {
-                CoroutineScope(Dispatchers.IO).launch {
-                    val res = ApiUtilities.getApiInterface()!!.getApptomative(value)
-                    withContext(Dispatchers.Main) {
-                        try {
-                            val result = res.body()?.adscode
-                            Log.d("resultData", result.toString())
-                            Prefs.setResponseAll(applicationContext, result)
+                try {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        val res = ApiUtilities.getApiInterface()!!.getApptomative(value)
+                        withContext(Dispatchers.Main) {
+                            try {
+                                val result = res.body()?.adscode
+                                Log.d("resultData", result.toString())
+                                Prefs.setResponseAll(applicationContext, result)
 
 
-                        } catch (e: Exception) {
+                            } catch (e: Exception) {
 
 
+                            }
                         }
                     }
+                } catch (e: Exception) {
+
+
                 }
-            } catch (e: Exception) {
 
 
             }
 
-
+        } else {
+            //Toast.makeText(this, "No internet", Toast.LENGTH_SHORT).show()
         }
 
 
+
+
+
+    }
+
+    private fun checkForInternet(context: Context): Boolean {
+        val connectivityManager =  context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+
+            @Suppress("DEPRECATION") val networkInfo =
+                connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
     }
 
     /** LifecycleObserver method that shows the app open ad when the app moves to foreground. */
